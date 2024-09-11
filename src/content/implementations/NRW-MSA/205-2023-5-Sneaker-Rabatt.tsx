b77c8383023789e4cb0f7e69d950cb182a43cd70
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-09-11 18:03:08 +0200

    Fixes

--- src/content/implementations/NRW-MSA/205-2023-5-Sneaker-Rabatt.tsx
+++ src/content/implementations/NRW-MSA/205-2023-5-Sneaker-Rabatt.tsx
@@ -45,7 +45,7 @@ export const exercise205: Exercise<DATA> = {
   solution({ data }) {
     return (
       <>
-        <p>Wir berechnen den Verkaufspreis mit der Formel des Prozentwerts:</p>
+        <p>Wir berechnen zuerst den Rabatt mit der Formel des Prozentwerts:</p>
         <p>W = G · p</p>
 
         <p>
@@ -57,9 +57,13 @@ export const exercise205: Exercise<DATA> = {
           W = {data.preis} · {pp(data.rabatt / 100)}
         </p>
         <p>W = {pp((data.preis * data.rabatt) / 100)}</p>
+        <p>Der Rabatt beträgt {pp((data.preis * data.rabatt) / 100)} € </p>
         <p>
-          Der neue Verkaufspreis beträgt {pp((data.preis * data.rabatt) / 100)}{' '}
-          €{' '}
+          <br></br>Damit beträgt der neue Verkaufspreis:
+        </p>
+        <p>
+          {data.preis} − {pp((data.preis * data.rabatt) / 100)} ={' '}
+          {pp(data.preis - (data.preis * data.rabatt) / 100)}
         </p>
       </>
     )
