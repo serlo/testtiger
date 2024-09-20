ef5eee9d697ad9ad771b2e2a901d9c9e5d6c43ca
Mikey Stengel <mikey@serlo.org>
2024-09-20 17:10:22 +0200

    refactor(style): Change project name and add back important

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,22 @@ module.exports = {
       typography: {
         DEFAULT: {
           css: {
-            '--tw-prose-body': '#374151', // gray-700
-            '--tw-prose-headings': '#111827', // gray-900
-            '--tw-prose-lead': '#6B7280', // gray-500
-            '--tw-prose-links': '#2563EB', // blue-600
-            '--tw-prose-bold': '#111827', // gray-900
-            '--tw-prose-counters': '#4B5563', // gray-600
-            '--tw-prose-bullets': '#9CA3AF', // gray-400
-            '--tw-prose-hr': '#E5E7EB', // gray-200
-            '--tw-prose-quotes': '#111827', // gray-900
-            '--tw-prose-quote-borders': '#E5E7EB', // gray-200
-            '--tw-prose-captions': '#6B7280', // gray-500
-            '--tw-prose-code': '#111827', // gray-900
-            '--tw-prose-pre-code': '#F3F4F6', // gray-100
-            '--tw-prose-pre-bg': '#111827', // gray-900
-            '--tw-prose-th-borders': '#D1D5DB', // gray-300
-            '--tw-prose-td-borders': '#E5E7EB', // gray-200
+            '--tw-prose-body': '#374151 !important', // gray-700
+            '--tw-prose-headings': '#111827 !important', // gray-900
+            '--tw-prose-lead': '#6B7280 !important', // gray-500
+            '--tw-prose-links': '#2563EB !important', // blue-600
+            '--tw-prose-bold': '#111827 !important', // gray-900
+            '--tw-prose-counters': '#4B5563 !important', // gray-600
+            '--tw-prose-bullets': '#9CA3AF !important', // gray-400
+            '--tw-prose-hr': '#E5E7EB !important', // gray-200
+            '--tw-prose-quotes': '#111827 !important', // gray-900
+            '--tw-prose-quote-borders': '#E5E7EB !important', // gray-200
+            '--tw-prose-captions': '#6B7280 !important', // gray-500
+            '--tw-prose-code': '#111827 !important', // gray-900
+            '--tw-prose-pre-code': '#F3F4F6 !important', // gray-100
+            '--tw-prose-pre-bg': '#111827 !important', // gray-900
+            '--tw-prose-th-borders': '#D1D5DB !important', // gray-300
+            '--tw-prose-td-borders': '#E5E7EB !important', // gray-200
           },
         },
       },
