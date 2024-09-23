4e255d99360305ec9e4bb62148df796200325b13
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-09-23 13:31:40 +0200

    refactor 192

--- src/helper/pretty-print.tsx
+++ src/helper/pretty-print.tsx
@@ -1,10 +1,10 @@
 import { Fragment } from 'react'
+import { buildInlineFrac } from './math-builder'
+
+type Mode = 'normal' | 'embrace_neg' | 'merge_op' | 'koeff'
 
 /** pretty print a number */
-export function pp(
-  x: number,
-  mode: 'normal' | 'embrace_neg' | 'merge_op' = 'normal',
-): string {
+export function pp(x: number, mode: Mode = 'normal'): string {
   const numStr = Math.abs(x)
     .toLocaleString('de-De', { maximumFractionDigits: 7 })
     .replace(/\./g, '\xa0')
@@ -17,6 +17,18 @@ export function pp(
     return `(−${numStr})`
   }
   if (mode === 'merge_op') {
+    if (x >= 0) {
+      return `+ ${numStr}`
+    }
+    return `− ${numStr}`
+  }
+  if (mode === 'koeff') {
+    if (x == 1) {
+      return '+ '
+    }
+    if (x == -1) {
+      return '− '
+    }
     if (x >= 0) {
       return `+ ${numStr}`
     }
@@ -67,4 +79,51 @@ export function ppPolynom(polynom: [number, string, number][]): JSX.Element {
   )
 }
 
-export function ppFrac(n: number) {}
+export function ppFrac(n: number, mode: Mode = 'normal'): JSX.Element {
+  if (Number.isInteger(n)) {
+    return <>{pp(n, mode)}</>
+  }
+  let bestDenom = -1
+  let error = Infinity
+  for (let i = 2; i <= 100; i++) {
+    const e = Math.abs(n) * i - Math.floor(Math.abs(n) * i)
+    if (e < error) {
+      error = e
+      bestDenom = i
+    }
+  }
+  if (error < 0.00000001) {
+    const f = buildInlineFrac(Math.round(Math.abs(n) * bestDenom), bestDenom)
+    if (mode === 'normal') {
+      return (
+        <>
+          {n < 0 ? '−' : ''}
+          {f}
+        </>
+      )
+    }
+    if (mode === 'embrace_neg') {
+      if (n >= 0) return f
+      return <>(−{f})</>
+    }
+    if (mode === 'merge_op') {
+      if (n >= 0) {
+        return <>+ {f}</>
+      }
+      return <>− {f}</>
+    }
+    if (mode === 'koeff') {
+      if (n == 1) {
+        return <>+ </>
+      }
+      if (n == -1) {
+        return <>− </>
+      }
+      if (n >= 0) {
+        return <>+ {f}</>
+      }
+      return <>− {f}</>
+    }
+  }
+  return <>{pp(n, mode)}</>
+}
