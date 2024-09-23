4a090c1ef49d0e4d50f0ce95bfa23e9b5f1b2ca9
Alexandra-Wolfram <164320968+Alexandra-Wolfram@users.noreply.github.com>
2024-09-23 15:54:26 +0200

    Update 32-2019-1-2-Rechteck.tsx

--- src/content/implementations/NRW-MSA/32-2019-1-2-Rechteck.tsx
+++ src/content/implementations/NRW-MSA/32-2019-1-2-Rechteck.tsx
@@ -1,27 +1,47 @@
 import { Exercise } from '@/data/types'
 
-interface DATA {}
+interface DATA {
+  a: number
+  b: number
+  s: string
+  r: number
+}
 
 export const exercise32: Exercise<DATA> = {
-  title: 'NEU',
-  source: '',
+  title: 'Berechnungen am Rechteck',
+  source: '2019 Teil 1 /4',
   useCalculator: false,
   duration: 42,
   points: [42],
   generator(rng) {
-    return {}
+    return {
+      a: rng.randomIntBetween(3, 7),
+      b: rng.randomIntBetween(2, 5),
+      s: rng.randomItemFromArray([
+        'verdoppelt',
+        'verdreifacht',
+        'vervierfacht',
+        'drittelt',
+        'halbiert',
+      ]),
+      r: rng.randomIntBetween(1, 6) * 6,
+    }
   },
   constraint({ data }) {
-    return true
+    return data.a != data.b && data.a * data.b != data.r
   },
   subtasks: {
     intro: ({ data }) => {
-      return <></>
+      return (
+        <>
+          Ein Rechteck hat die Seitenlängen a = {data.a} cm und b = {data.b} cm.
+        </>
+      )
     },
     main: [
       {
         task({ data }) {
-          return <></>
+          return <>a) Berechne die Länge der Diagonale d.</>
         },
         solution({ data }) {
           return <></>
@@ -29,7 +49,26 @@ export const exercise32: Exercise<DATA> = {
       },
       {
         task({ data }) {
+          return (
+            <>
+              b) Wie verändert sich der Flächeninhalt dieses Rechtecks, wenn man
+              jede Seitenlänge {data.s}? Begründe.
+            </>
+          )
+        },
+        solution({ data }) {
           return <></>
+        },
+      },
+      {
+        task({ data }) {
+          return (
+            <>
+              c) Ein anderes Rechteck hat einen Flächeninhalt von {data.r} cm².
+              Wie lang könnten die Seiten sein? Gib zwei unterschiedliche
+              Möglichkeiten an.
+            </>
+          )
         },
         solution({ data }) {
           return <></>
