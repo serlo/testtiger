4bb4c7c742bb1dc73e8bc16890434fcd4dae12eb
Alexandra-Wolfram <164320968+Alexandra-Wolfram@users.noreply.github.com>
2024-09-23 11:30:02 +0200

    new exercise

---
+++ src/content/implementations/NRW-MSA/26-2021-v2-3-volumen-preis.tsx
@@ -0,0 +1,39 @@
+import { Exercise } from '@/data/types'
+
+interface DATA {}
+
+export const exerciseXXX: Exercise<DATA> = {
+  title: 'NEU',
+  useCalculator: false,
+  duration: 42,
+  points: [42],
+  generator(rng) {
+    return {}
+  },
+  constraint({ data }) {
+    return true
+  },
+  subtasks: {
+    intro: ({ data }) => {
+      return <></>
+    },
+    main: [
+      {
+        task({ data }) {
+          return <></>
+        },
+        solution({ data }) {
+          return <></>
+        },
+      },
+      {
+        task({ data }) {
+          return <></>
+        },
+        solution({ data }) {
+          return <></>
+        },
+      },
+    ],
+  },
+}
