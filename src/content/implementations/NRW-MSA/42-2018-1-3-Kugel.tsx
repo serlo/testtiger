a5571d7e34011bd31f03fc3ab1057de6ac06646a
Alexandra-Wolfram <164320968+Alexandra-Wolfram@users.noreply.github.com>
2024-09-27 11:31:29 +0200

    Update 42-2018-1-3-Kugel.tsx

--- src/content/implementations/NRW-MSA/42-2018-1-3-Kugel.tsx
+++ src/content/implementations/NRW-MSA/42-2018-1-3-Kugel.tsx
@@ -1,6 +1,12 @@
 import { Exercise } from '@/data/types'
+import { pp } from '@/helper/pretty-print'
+import { roundToDigits } from '@/helper/round-to-digits'
 
-interface DATA {}
+interface DATA {
+  r: number
+  case: number
+  right: number
+}
 
 export const exercise42: Exercise<DATA> = {
   title: 'Kugel',
@@ -8,31 +14,83 @@ export const exercise42: Exercise<DATA> = {
   useCalculator: false,
   duration: 42,
   generator(rng) {
-    return {}
+    return {
+      r: rng.randomIntBetween(3, 8),
+      case: rng.randomIntBetween(0, 2),
+      right: rng.randomIntBetween(0, 1),
+    }
   },
   constraint({ data }) {
     return true
   },
   intro({ data }) {
-    return <></>
+    return (
+      <>
+        <p>TODO: Abbildung Kugel</p>
+        <p>Eine Kugel hat einen Radius von {data.r} cm.</p>
+      </>
+    )
   },
   tasks: [
     {
       points: 42,
       task({ data }) {
-        return <></>
+        return (
+          <>
+            <p>a) Berechne die Oberfläche der Kugel.</p>
+          </>
+        )
       },
       solution({ data }) {
-        return <></>
+        return (
+          <>
+            <p>Berechne die Oberfläche der Kugel mit der Formel:</p>
+            <p>O = 4 · π · r²</p>
+            <p>O = 4 · π · {data.r}²</p>
+            <p>
+              O = {pp(roundToDigits(4 * Math.PI * Math.pow(data.r, 2), 2))} cm²
+            </p>
+          </>
+        )
       },
     },
     {
       points: 42,
       task({ data }) {
-        return <></>
+        const text1 = ['verdopple', 'verdreifache', 'halbiere']
+        const text2 = ['verdoppelt', 'verdreifacht', 'halbiert']
+        const text2korrekt = ['vervierfacht', 'verneunfacht', 'viertelt']
+
+        return (
+          <>
+            <p>
+              b) Sina überlegt: Wenn ich den Radius {text1[data.case]}, <p />
+              <p>Hat Sina recht? Begründe deine Entscheidung.</p>
+            </p>
+          </>
+        )
       },
       solution({ data }) {
-        return <></>
+        const text3 = ['doppelt', 'dreifach', 'vierfach', 'halb']
+        const faktor = [2, 3, 4, 0.5]
+        return (
+          <>
+            <p>
+              Setze den {text3[data.case]}en Radius in die Formel für die
+              Oberfläche ein:
+            </p>
+            <p>O = 4 · π · r²</p>
+            <p>
+              O = 4 · π · ({faktor[data.case]} · {data.r})²
+            </p>
+            <p>
+              O = 4 · π · {faktor[data.case]}² · {data.r}²
+            </p>
+            <p>
+              O = 4 · π · {faktor[data.case] * faktor[data.case]} · {data.r}²
+            </p>
+          </>
+        )
       },
     },
   ],
