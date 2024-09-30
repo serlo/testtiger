f69b1fb162d2ee2f3f2a068ed65e632507bf7e5e
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-09-30 13:16:31 +0200

    increase file limit

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import express from 'express'
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json({ limit: '20mb' }))
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // manage CORS
