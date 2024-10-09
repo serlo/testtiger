09d2e5a2e9f6254f886d850e819b01104e620bd0
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-10-09 14:23:04 +0200

    Update 40-2018-1-1-Zahlen ordnen.tsx

--- src/content/implementations/NRW-MSA/40-2018-1-1-Zahlen ordnen.tsx
+++ src/content/implementations/NRW-MSA/40-2018-1-1-Zahlen ordnen.tsx
@@ -1,5 +1,8 @@
 import { Exercise } from '@/data/types'
+import { getGcd } from '@/helper/get-gcd'
+import { buildInlineFrac } from '@/helper/math-builder'
 import { pp, ppFrac } from '@/helper/pretty-print'
+import { roundToDigits } from '@/helper/round-to-digits'
 
 interface DATA {
   a: number
@@ -20,19 +23,19 @@ export const exercise40: Exercise<DATA> = {
   duration: 4,
   generator(rng) {
     return {
-      a: rng.randomIntBetween(1, 9) / 10,
-      b: rng.randomIntBetween(1, 9),
+      a: rng.randomIntBetween(-9, 9) / 10,
+      b: rng.randomIntBetween(-9, 9),
       c: rng.randomItemFromArray([10, 20, 100]),
       d: rng.randomItemFromArray([1, 2, 4, 5, 8]),
       e: rng.randomItemFromArray([3, 6, 7, 9]),
-      f: rng.randomIntBetween(1, 99) / 100,
+      f: rng.randomIntBetween(-99, 99) / 100,
       g: rng.randomItemFromArray([55, 65, 75]),
       h: rng.randomItemFromArray([5, 15, 25]),
       i: rng.randomItemFromArray([30, 40, 60]),
     }
   },
   constraint({ data }) {
-    return true
+    return data.f != 0 && data.a != 0 && data.b != 0
   },
   intro({ data }) {
     return <></>
@@ -46,52 +49,47 @@ export const exercise40: Exercise<DATA> = {
             <p>
               a) Ordne die Zahlen der Größe nach. Beginne mit der kleinsten
               Zahl.
-              <br></br>-{pp(data.a)} &nbsp;&nbsp;&nbsp;&nbsp;{' '}
-              {ppFrac([data.b, data.c])} &nbsp;&nbsp;&nbsp;&nbsp; -
-              {ppFrac([data.d, data.e])} &nbsp;&nbsp;&nbsp;&nbsp; {pp(data.f)}
+              <br></br>
+              {pp(data.a)} &nbsp;&nbsp;&nbsp;&nbsp; {ppFrac([data.b, data.c])}{' '}
+              &nbsp;&nbsp;&nbsp;&nbsp; {ppFrac([data.d, data.e])}{' '}
+              &nbsp;&nbsp;&nbsp;&nbsp; {pp(data.f)}
             </p>
           </>
         )
       },
       solution({ data }) {
-        const array = [-data.a, -data.d / data.e, data.b / data.c, data.f].sort(
+        const array = [data.a, data.d / data.e, data.b / data.c, data.f].sort(
           (a, b) => a - b,
         )
+        const gerundet = roundToDigits(data.d / data.e, 2)
+
         return (
           <>
             <p>
-              Es sind zwei positive und zwei negative Zahlen gegeben. Du musst
-              also die negativen Zahlen miteinander vergleichen, und die
-              positiven miteinander.
+              Wandle {ppFrac([data.b, data.c])} und {ppFrac([data.d, data.e])}{' '}
+              jeweils in eine Dezimalzahl um. Dazu kannst du einen Bruch
+              beispielsweise als Division auffassen:
               <br></br>
+              {ppFrac([data.d, data.e])} = {data.d} : {data.e} ≈ {pp(gerundet)}
               <br></br>
-              Fangen wir mit den negativen Zahlen an: Hier bietet es sich an, -
-              {pp(data.a)} in einen Bruch umzuwandeln:
+              <p>
+                Oder verwenden, dass Brüche mit 10 oder 100 als Nenner direkt
+                als Dezimalzahlen übersetzt werden können:
+              </p>
+              {ppFrac([data.b, data.c])}{' '}
+              {data.c == 20 && (
+                <>
+                  = {buildInlineFrac(data.b + ' · 5', data.c + ' · 5')} ={' '}
+                  {ppFrac([data.b * 5, data.c * 5])}
+                </>
+              )}{' '}
+              = {pp(data.b / data.c)}
               <br></br>
-              {pp(-data.a)} = {ppFrac([-data.a * 10, 10])}
-              <br></br>
-              Jetzt musst du die beiden Brüche auf einen gemeinsamen Nenner
-              bringen. Der gemeinsame Nenner ist {data.e * 10}.<br></br>
-              <li>
-                {ppFrac([-data.a * 10, 10])} =
-                {ppFrac([-data.a * 10 * data.e, 10 * data.e])}
-              </li>
-              <li>
-                {ppFrac([-data.d, data.e])} =
-                {ppFrac([-data.d * 10, data.e * 10])}
-              </li>
-              Es ist also {ppFrac(array[0])} {' < '} {ppFrac(array[1])}.
-              <br></br>
-              <br></br>
-              Jetzt schauen wir uns die positiven Zahlen an. Hier bietet es sich
-              an, {ppFrac([data.b, data.c])} in eine Dezimalzahl umzuwandeln:
-              <br></br>
-              {ppFrac([data.b, data.c])} = {pp(data.b / data.c)}
-              <br></br>
-              Es ist also: {pp(array[2])} {' < '} {pp(array[3])}.<br></br>
-              <br></br>
-              <b>Antwort:</b> {ppFrac(array[0])} {' < '} {ppFrac(array[1])}{' '}
-              {' < '} {pp(array[2])} {' < '} {pp(array[3])}
+              <b>Antwort:</b>{' '}
+              {pp(array[0] == data.d / data.e ? gerundet : array[0])} {' < '}{' '}
+              {pp(array[1] == data.d / data.e ? gerundet : array[1])} {' < '}{' '}
+              {pp(array[2] == data.d / data.e ? gerundet : array[2])} {' < '}{' '}
+              {pp(array[3] == data.d / data.e ? gerundet : array[3])}
             </p>
           </>
         )
