171bdd8068f19cac2c06ef3fbfcc29299403f220
alinegeissler <aline-geissler@web.de>
2024-10-23 15:59:05 +0200

    Create 100-2023-1-2.tsx

---
+++ src/content/implementations/NRW-MSA/100-2023-1-2.tsx
@@ -0,0 +1,23 @@
+import { Exercise } from '@/data/types'
+
+interface DATA {}
+
+export const exercise100: Exercise<DATA> = {
+  title: 'Einheiten umrechnen',
+  source: '2023 Teil 1 Aufgabe 2',
+  useCalculator: false,
+  duration: 42,
+  points: 42,
+  generator(rng) {
+    return {}
+  },
+  constraint({ data }) {
+    return true
+  },
+  task({ data }) {
+    return <></>
+  },
+  solution({ data }) {
+    return <></>
+  },
+}
