ab8946d3a2b726b88e0769dd62f3417bf26f97c8
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-10-24 13:17:33 +0200

    Update 25-2021-v2-2-zahlen-ordnen.tsx

--- src/content/implementations/NRW-MSA/25-2021-v2-2-zahlen-ordnen.tsx
+++ src/content/implementations/NRW-MSA/25-2021-v2-2-zahlen-ordnen.tsx
@@ -41,95 +41,86 @@ export const exercise25: Exercise<DATA> = {
       array[2] != array[3]
     )
   },
-  intro({ data }) {
-    return <></>
+  points: 2,
+  task({ data }) {
+    return (
+      <>
+        <p>Ordne die Zahlen der Größe nach. Beginne mit der kleinsten Zahl.</p>
+
+        <p>
+          {ppFrac([data.c, data.d])}&nbsp;&nbsp;&nbsp;&nbsp; {pp(data.b)}
+          &nbsp;&nbsp;&nbsp;&nbsp; {pp(data.e)}
+          <sup>-1</sup>
+          &nbsp;&nbsp;&nbsp;&nbsp; {pp(data.a)}
+        </p>
+      </>
+    )
   },
-  tasks: [
-    {
-      points: 2,
-      task({ data }) {
-        return (
-          <>
-            <p>
-              Ordne die Zahlen der Größe nach. Beginne mit der kleinsten Zahl.
-            </p>
+  solution({ data }) {
+    const potenz = Math.pow(data.e, -1)
+    const array = [data.a, data.b, data.c / data.d, potenz].sort(
+      (a, b) => a - b,
+    )
+    return (
+      <>
+        <p>
+          Wandle den Bruch zuerst in eine Dezimalzahl um, um ihn mit den anderen
+          Zahlen vergleichen zu können:
+        </p>
 
-            <p>
-              {ppFrac([data.c, data.d])}&nbsp;&nbsp;&nbsp;&nbsp; {pp(data.b)}
-              &nbsp;&nbsp;&nbsp;&nbsp; {pp(data.e)}
-              <sup>-1</sup>
-              &nbsp;&nbsp;&nbsp;&nbsp; {pp(data.a)}
-            </p>
-          </>
-        )
-      },
-      solution({ data }) {
-        const potenz = Math.pow(data.e, -1)
-        const array = [data.a, data.b, data.c / data.d, potenz].sort(
-          (a, b) => a - b,
-        )
-        return (
-          <>
-            <p>
-              Wandle den Bruch zuerst in eine Dezimalzahl um, um ihn mit den
-              anderen Zahlen vergleichen zu können:
-            </p>
+        <p>
+          {ppFrac([data.c, data.d])} = {pp(data.c / data.d)}
+        </p>
 
-            <p>
-              {ppFrac([data.c, data.d])} = {pp(data.c / data.d)}
-            </p>
+        <p>Forme auch die Potenz in eine Dezimalzahl um:</p>
 
-            <p>Forme auch die Potenz in eine Dezimalzahl um:</p>
+        <p>
+          {pp(data.e)}
+          <sup>-1</sup> = {ppFrac(1 / data.e)} = {pp(1 / data.e)}
+        </p>
 
-            <p>
-              {pp(data.e)}
-              <sup>-1</sup> = {ppFrac(1 / data.e)} = {pp(1 / data.e)}
-            </p>
+        <p>Ordne die Zahlen mit dem Operator {'"<"'}:</p>
 
-            <p>Ordne die Zahlen mit dem Operator {'"<"'}:</p>
-
-            <p>
-              <strong>
-                {(array[0] == data.c / data.d && ppFrac(array[0])) ||
-                  (array[0] == 1 / data.e && (
-                    <>
-                      {pp(data.e)}
-                      <sup>-1</sup>
-                    </>
-                  )) ||
-                  pp(array[0])}{' '}
-                {' < '}{' '}
-                {(array[1] == data.c / data.d && ppFrac(array[1])) ||
-                  (array[1] == 1 / data.e && (
-                    <>
-                      {pp(data.e)}
-                      <sup>-1</sup>
-                    </>
-                  )) ||
-                  pp(array[1])}{' '}
-                {' < '}{' '}
-                {(array[2] == data.c / data.d && ppFrac(array[2])) ||
-                  (array[2] == 1 / data.e && (
-                    <>
-                      {pp(data.e)}
-                      <sup>-1</sup>
-                    </>
-                  )) ||
-                  pp(array[2])}{' '}
-                {' < '}{' '}
-                {(array[3] == data.c / data.d && ppFrac(array[3])) ||
-                  (array[3] == 1 / data.e && (
-                    <>
-                      {pp(data.e)}
-                      <sup>-1</sup>
-                    </>
-                  )) ||
-                  pp(array[3])}
-              </strong>
-            </p>
-          </>
-        )
-      },
-    },
-  ],
+        <p>
+          <strong>
+            {(array[0] == data.c / data.d && ppFrac(array[0])) ||
+              (array[0] == 1 / data.e && (
+                <>
+                  {pp(data.e)}
+                  <sup>-1</sup>
+                </>
+              )) ||
+              pp(array[0])}{' '}
+            {' < '}{' '}
+            {(array[1] == data.c / data.d && ppFrac(array[1])) ||
+              (array[1] == 1 / data.e && (
+                <>
+                  {pp(data.e)}
+                  <sup>-1</sup>
+                </>
+              )) ||
+              pp(array[1])}{' '}
+            {' < '}{' '}
+            {(array[2] == data.c / data.d && ppFrac(array[2])) ||
+              (array[2] == 1 / data.e && (
+                <>
+                  {pp(data.e)}
+                  <sup>-1</sup>
+                </>
+              )) ||
+              pp(array[2])}{' '}
+            {' < '}{' '}
+            {(array[3] == data.c / data.d && ppFrac(array[3])) ||
+              (array[3] == 1 / data.e && (
+                <>
+                  {pp(data.e)}
+                  <sup>-1</sup>
+                </>
+              )) ||
+              pp(array[3])}
+          </strong>
+        </p>
+      </>
+    )
+  },
 }
