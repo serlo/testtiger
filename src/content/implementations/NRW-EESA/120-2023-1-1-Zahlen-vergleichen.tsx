d0f60f94a0c63ee82962729b7537c2b6b74d6ff9
alinegeissler <aline-geissler@web.de>
2024-10-25 10:55:30 +0200

    Update 120-2023-1-1-Zahlen-vergleichen.tsx

--- src/content/implementations/NRW-EESA/120-2023-1-1-Zahlen-vergleichen.tsx
+++ src/content/implementations/NRW-EESA/120-2023-1-1-Zahlen-vergleichen.tsx
@@ -1,6 +1,17 @@
 import { Exercise } from '@/data/types'
+import { pp, ppFrac } from '@/helper/pretty-print'
 
-interface DATA {}
+interface DATA {
+  a: number
+  b: number
+  c: number
+  d: number
+  e: number
+  f: number
+  g: number
+  h: number
+  i: number
+}
 
 export const exercise120: Exercise<DATA> = {
   title: 'Zahlen vergleichen',
@@ -9,13 +20,31 @@ export const exercise120: Exercise<DATA> = {
   duration: 42,
   points: 42,
   generator(rng) {
-    return {}
+    return {
+      a: rng.randomIntBetween(10, 100) / -100,
+      b: rng.randomIntBetween(10, 100) / -100,
+      c: rng.randomItemFromArray([5, 10, 20, 30, 40, 50, 60, 70, 80, 90]),
+      d: rng.randomItemFromArray([5, 10, 50, 100]),
+      e: rng.randomIntBetween(1, 9),
+      f: rng.randomItemFromArray([2, 4, 5]),
+      g: rng.randomIntBetween(100, 100) / 100,
+      h: rng.randomIntBetween(1, 9),
+      i: rng.randomItemFromArray([2, 4, 5]),
+    }
   },
   constraint({ data }) {
     return true
   },
   task({ data }) {
-    return <></>
+    return (
+      <>
+        <p>
+          Vergleiche und setze in die Lücke jeweils das Zeichen "{'<'}", "{'>'}"
+          oder "{'='}" ein.
+        </p>
+        <p>{pp(data.a)}</p>
+      </>
+    )
   },
   solution({ data }) {
     return <></>
