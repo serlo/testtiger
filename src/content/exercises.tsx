50226c0a24bac1bc30add016ec8c9d44cd460fa7
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-10-29 10:08:08 +0100

    Aufgabe fertiggestellt

--- src/content/exercises.tsx
+++ src/content/exercises.tsx
@@ -64,6 +64,9 @@ import { exercise126 } from './implementations/NRW-EESA/126-2023-2-1-Pizzeria'
 import { exercise127 } from './implementations/NRW-EESA/127-2023-2-2-Tischgruppe'
 import { exercise128 } from './implementations/NRW-EESA/128-2023-2-3-Zugfahrt'
 import { exercise135 } from './implementations/NRW-EESA/135-2024-2-1-Garten'
+import { exercise117 } from './implementations/NRW-EESA/117-2022-2-1-Hochbeet'
+import { exercise118 } from './implementations/NRW-EESA/118-2022-2-2-Führerschein'
+import { exercise119 } from './implementations/NRW-EESA/119-2022-2-3-Bambus'
 
 export const exercisesData: { [key: number]: Exercise<any> } = {
   1: exercise1,
@@ -130,6 +133,9 @@ export const exercisesData: { [key: number]: Exercise<any> } = {
   126: exercise126,
   127: exercise127,
   128: exercise128,
+  117: exercise117,
+  118: exercise118,
+  119: exercise119,
 }
 
 // symbols:
