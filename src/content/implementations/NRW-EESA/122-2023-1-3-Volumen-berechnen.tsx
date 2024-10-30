76038d3ee34ef0f372b1bbc8be77054c7c32787c
alinegeissler <aline-geissler@web.de>
2024-10-30 17:17:57 +0100

    122

--- src/content/implementations/NRW-EESA/122-2023-1-3-Volumen-berechnen.tsx
+++ src/content/implementations/NRW-EESA/122-2023-1-3-Volumen-berechnen.tsx
@@ -1,6 +1,11 @@
 import { Exercise } from '@/data/types'
+import { pp } from '@/helper/pretty-print'
 
-interface DATA {}
+interface DATA {
+  length: number
+  width: number
+  height: number
+}
 
 export const exercise122: Exercise<DATA> = {
   title: 'Volumen berechnen',
@@ -9,15 +14,61 @@ export const exercise122: Exercise<DATA> = {
   duration: 42,
   points: 42,
   generator(rng) {
-    return {}
+    return {
+      length: rng.randomIntBetween(25, 35),
+      width: rng.randomIntBetween(15, 25),
+      height: rng.randomIntBetween(5, 15),
+    }
+  },
+  originalData: {
+    length: 30,
+    width: 20,
+    height: 6,
   },
   constraint({ data }) {
     return true
   },
   task({ data }) {
-    return <></>
+    return (
+      <>
+        <p>Berechne das Volumen des abgebildeten Kartons.</p>
+        <svg viewBox="0 0 667 341">
+          <image
+            href="/content/NRW_EESA/122_Volumen.jpg"
+            height="341"
+            width="667"
+          />
+          <text x={150} y={250} fontSize={30} textAnchor="right" stroke="black">
+            {pp(data.length)} cm
+          </text>
+          <text x={460} y={250} fontSize={30} textAnchor="right" stroke="black">
+            {pp(data.width)} cm
+          </text>
+          <text x={570} y={125} fontSize={30} textAnchor="right" stroke="black">
+            {pp(data.height)} cm
+          </text>
+        </svg>
+      </>
+    )
   },
   solution({ data }) {
-    return <></>
+    return (
+      <>
+        <p>Berechne das Volumen des quaderförmigen Kartons mit der Formel:</p>
+        <p>
+          <b>
+            V<sub>Quader</sub> = l · b · h{' '}
+          </b>
+          = {pp(data.length)} · {pp(data.width)} · {pp(data.height)} ={' '}
+          {data.length * data.width * data.height} cm<sup>3</sup>
+        </p>
+        <p>
+          <b>
+            Antwort: Das Volumen des Kartons beträgt{' '}
+            {data.length * data.width * data.height} cm<sup>3</sup>.
+          </b>
+        </p>
+      </>
+    )
   },
 }
