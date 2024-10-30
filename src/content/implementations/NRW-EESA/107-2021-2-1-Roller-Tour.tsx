03b36d5c75a5a40a4071449e60c0f2eb43f22149
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-10-30 13:32:23 +0100

    Aufgabe weitergeführt

--- src/content/implementations/NRW-EESA/107-2021-2-1-Roller-Tour.tsx
+++ src/content/implementations/NRW-EESA/107-2021-2-1-Roller-Tour.tsx
@@ -1,4 +1,5 @@
 import { Exercise } from '@/data/types'
+import { Color5 } from '@/helper/colors'
 
 interface DATA {}
 
@@ -22,6 +23,21 @@ export const exercise107: Exercise<DATA> = {
           eine Woche unterwegs sein. Am ersten Tag wollen sie von Fröndenberg zu
           einem Campingplatz am Möhnesee fahren (Abbildung 1).
         </p>
+        <svg viewBox="0 0 328 130">
+          <image
+            href="/content/NRW_EESA/107_Roller-Tour.PNG"
+            height="130"
+            width="328"
+          />
+        </svg>
+        <center>
+          <Color5>
+            <span style={{ fontSize: 'small' }}>
+              Abbildung 1: geplante Strecke von Fröndenberg zum Campingplatz am
+              Möhnesee
+            </span>
+          </Color5>
+        </center>
       </>
     )
   },
@@ -32,7 +48,14 @@ export const exercise107: Exercise<DATA> = {
         return <></>
       },
       task({ data }) {
-        return <></>
+        return (
+          <>
+            <p>
+              a) Die Strecke ist ungefähr 30 km lang. Bestätige dies durch
+              Messen und mithilfe des Maßstabs.
+            </p>
+          </>
+        )
       },
       solution({ data }) {
         return <></>
@@ -44,7 +67,108 @@ export const exercise107: Exercise<DATA> = {
         return <></>
       },
       task({ data }) {
+        return (
+          <>
+            <p>
+              b) Melike und Robin gehen von einer durchschnittlichen
+              Geschwindigkeit von 24 hkm​ aus. Berechne für die 30 km lange
+              Strecke die Fahrzeit in Minuten. Notiere deinen Lösungsweg.
+            </p>
+          </>
+        )
+      },
+      solution({ data }) {
         return <></>
+      },
+    },
+    {
+      points: 42,
+      intro({ data }) {
+        return (
+          <>
+            <p>
+              Für die Tour wollen Melike und Robin einen Motorroller für eine
+              Woche mieten. Robin findet zwei Angebote (Abbildung 2).
+            </p>
+            <svg viewBox="0 0 328 110">
+              <image
+                href="/content/NRW_EESA/107_Roller-Tour4.PNG"
+                height="110"
+                width="328"
+              />
+            </svg>
+            <svg viewBox="0 0 328 110">
+              <image
+                href="/content/NRW_EESA/107_Roller-Tour5.PNG"
+                height="110"
+                width="328"
+              />
+            </svg>
+            <center>
+              <Color5>
+                <span style={{ fontSize: 'small' }}>
+                  Abbildung 2: Angebote der Rollervermietungen {'"'}Easy Rent
+                  {'"'} und {'"'}Rollerverleih24{'"'}
+                </span>
+              </Color5>
+            </center>
+          </>
+        )
+      },
+      task({ data }) {
+        return (
+          <>
+            <p>
+              c) Die Kosten bei {'"'}Easy Rent{'"'} können mit dem Term 0,2 ⋅ x
+              + 80 berechnet werden
+            </p>
+            <p>
+              Gib die Bedeutung von x, von 0,2 und von 80 im Zusammenhang mit
+              den Kosten an.
+            </p>
+          </>
+        )
+      },
+      solution({ data }) {
+        return <></>
+      },
+    },
+    {
+      points: 42,
+      intro({ data }) {
+        return (
+          <>
+            <p>
+              Melike und Robin wollen die Angebote miteinander vergleichen und
+              erstellen dazu eine Grafik (Abbildung 3).
+            </p>
+            <svg viewBox="0 0 328 220">
+              <image
+                href="/content/NRW_EESA/107_Roller-Tour3.PNG"
+                height="220"
+                width="328"
+              />
+            </svg>
+            <center>
+              <Color5>
+                <span style={{ fontSize: 'small' }}>
+                  Abbildung 3: Vergleich der Angebote von {'"'}Easy Rent
+                  {'"'} und {'"'}Rollerverleih24{'"'}
+                </span>
+              </Color5>
+            </center>
+          </>
+        )
+      },
+      task({ data }) {
+        return (
+          <>
+            <p>
+              d) Erkläre, warum der Graph zum Angebot von {'"'}Rollerverleih24
+              {'"'} im ersten Abschnitt waagerecht verläuft.
+            </p>
+          </>
+        )
       },
       solution({ data }) {
         return <></>
@@ -56,7 +180,14 @@ export const exercise107: Exercise<DATA> = {
         return <></>
       },
       task({ data }) {
-        return <></>
+        return (
+          <>
+            <p>
+              e) Die beiden Graphen schneiden sich. Gib die Koordinaten der
+              beiden Schnittpunkte an.
+            </p>
+          </>
+        )
       },
       solution({ data }) {
         return <></>
@@ -68,31 +199,15 @@ export const exercise107: Exercise<DATA> = {
         return <></>
       },
       task({ data }) {
-        return <></>
-      },
-      solution({ data }) {
-        return <></>
-      },
-    },
-    {
-      points: 42,
-      intro({ data }) {
-        return <></>
-      },
-      task({ data }) {
-        return <></>
-      },
-      solution({ data }) {
-        return <></>
-      },
-    },
-    {
-      points: 42,
-      intro({ data }) {
-        return <></>
-      },
-      task({ data }) {
-        return <></>
+        return (
+          <>
+            <p>
+              f) Insgesamt wollen Melike und Robin ungefähr 170 km weit fahren.
+              Welches Angebot ist günstiger? Gib die Kosten für das günstigere
+              Angebot an.
+            </p>
+          </>
+        )
       },
       solution({ data }) {
         return <></>
--- src/content/implementations/NRW-EESA/109-2021-2-3-Kasimir.tsx
+++ src/content/implementations/NRW-EESA/109-2021-2-3-Kasimir.tsx
@@ -1,21 +1,66 @@
 import { Exercise } from '@/data/types'
+import { Color5 } from '@/helper/colors'
+import { buildInlineFrac, buildSqrt } from '@/helper/math-builder'
 
-interface DATA {}
+interface DATA {
+  item_1: number
+  item_2: number
+  item_3: number
+  order: Array<number>
+}
+const richtig = [
+  '/content/NRW_EESA/109_Kasimir6.PNG',
+  '/content/NRW_EESA/109_Kasimir10.PNG',
+  ,
+]
 
+const falsch = [
+  '/content/NRW_EESA/109_Kasimir4.PNG',
+  '/content/NRW_EESA/109_Kasimir5.PNG',
+  '/content/NRW_EESA/109_Kasimir7.PNG',
+  '/content/NRW_EESA/109_Kasimir8.PNG',
+  '/content/NRW_EESA/109_Kasimir9.PNG',
+]
 export const exercise109: Exercise<DATA> = {
   title: 'Kasimir bastelt',
   source: '2021 Teil 2 Aufgabe 3',
   useCalculator: true,
   duration: 30,
   generator(rng) {
-    return {}
+    return {
+      item_1: rng.randomIntBetween(0, 1),
+      item_2: rng.randomIntBetween(0, 4),
+      item_3: rng.randomIntBetween(0, 4),
+      order: rng.shuffleArray([0, 1, 2]),
+    }
   },
-  originalData: {},
+  originalData: { item_1: 1, item_2: 0, item_3: 0, order: [2, 1, 0] },
   constraint({ data }) {
-    return true
+    return data.item_2 != data.item_3
   },
   intro({ data }) {
-    return null
+    return (
+      <>
+        <p>
+          Kasimir zeichnet ein gleichseitiges Dreieck mit der Seitenlänge a = 10
+          cm und einer Höhe h ˜ 8,7 cm (Abbildung 1).
+        </p>
+        <svg viewBox="0 0 328 180">
+          <image
+            href="/content/NRW_EESA/109_Kasimir1.PNG"
+            height="180"
+            width="328"
+          />
+        </svg>
+        <center>
+          <Color5>
+            <span style={{ fontSize: 'small' }}>
+              Abbildung 1: gleichseitiges Dreieck
+            </span>
+          </Color5>
+        </center>
+      </>
+    )
   },
   tasks: [
     {
@@ -24,7 +69,14 @@ export const exercise109: Exercise<DATA> = {
         return null
       },
       task({ data }) {
-        return <></>
+        return (
+          <>
+            <p>
+              a) Bestätige durch eine Rechnung, dass der Flächeninhalt des
+              Dreiecks 43,5 cm² beträgt.
+            </p>
+          </>
+        )
       },
       solution({ data }) {
         return <></>
@@ -33,10 +85,165 @@ export const exercise109: Exercise<DATA> = {
     {
       points: 42,
       intro({ data }) {
-        return null
+        return (
+          <>
+            <p>
+              Kasimir markiert auf jeder Dreieckseite den Mittelpunkt. Die
+              Mittelpunkte verbindet er. Es entstehen vier gleiche kleine
+              Dreiecke (Abbildung 2).
+            </p>
+            <svg viewBox="0 0 328 180">
+              <image
+                href="/content/NRW_EESA/109_Kasimir2.PNG"
+                height="180"
+                width="328"
+              />
+            </svg>
+            <center>
+              <Color5>
+                <span style={{ fontSize: 'small' }}>
+                  Abbildung 2: Einteilung des Dreiecks
+                </span>
+              </Color5>
+            </center>
+          </>
+        )
       },
       task({ data }) {
+        return (
+          <>
+            <p>
+              Zeichne die Figur (Abbildung 2) mit den Originalmaßen auf ein
+              DIN-A4-Blatt.
+            </p>
+          </>
+        )
+      },
+      solution({ data }) {
         return <></>
+      },
+    },
+    {
+      points: 42,
+      intro({ data }) {
+        return <></>
+      },
+      task({ data }) {
+        return (
+          <>
+            <p>
+              Bestätige durch eine Rechnung, dass der Flächeninhalt eines
+              kleinen Dreiecks ca. 10,9 cm² beträgt.
+            </p>
+          </>
+        )
+      },
+      solution({ data }) {
+        return <></>
+      },
+    },
+    {
+      points: 42,
+      intro({ data }) {
+        return (
+          <>
+            <p>
+              Kasimir klappt die drei äußeren hellen Dreiecke nach oben. Es
+              entsteht eine besondere Pyramide, die Tetraeder genannt wird
+              (Abbildung 3). Die Kantenlänge des Tetraeders beträgt 5 cm. Der
+              Flächeninhalt der Grundfläche beträgt ca. 10,9 cm².
+            </p>
+            <svg viewBox="0 0 328 180">
+              <image
+                href="/content/NRW_EESA/109_Kasimir3.PNG"
+                height="180"
+                width="328"
+              />
+            </svg>
+            <center>
+              <Color5>
+                <span style={{ fontSize: 'small' }}>
+                  Abbildung 3: Tetraeder
+                </span>
+              </Color5>
+            </center>
+            <p>
+              Die Körperhöhe h<sub>K​</sub> eines Tetraeders kann mit folgender
+              Formel berechnet werden:
+            </p>
+            <p>
+              h<sub>K​</sub> = {buildInlineFrac(<>{buildSqrt(6)}</>, 3)} · b
+            </p>
+          </>
+        )
+      },
+      task({ data }) {
+        return (
+          <>
+            <p>
+              Zeige mit der Formel, dass h<sub>K​</sub> ​≈ 4,1 cm lang ist.
+            </p>
+          </>
+        )
+      },
+      solution({ data }) {
+        return <></>
+      },
+    },
+    {
+      points: 42,
+      intro({ data }) {
+        return <></>
+      },
+      task({ data }) {
+        return (
+          <>
+            <p>
+              Berechne mit der Körperhöhe h<sub>K​</sub>​ das Volumen des
+              Tetraeders.
+            </p>
+          </>
+        )
+      },
+      solution({ data }) {
+        return <></>
+      },
+    },
+    {
+      points: 42,
+      intro({ data }) {
+        return <></>
+      },
+      task({ data }) {
+        const listItems = [
+          <li key="1">
+            <svg viewBox="0 0 328 120">
+              <image href={richtig[data.item_1]} height="120" width="328" />
+            </svg>
+          </li>,
+          <li key="2">
+            <svg viewBox="0 0 328 120">
+              <image href={falsch[data.item_2]} height="120" width="328" />
+            </svg>
+          </li>,
+
+          <li key="3">
+            <svg viewBox="0 0 328 120">
+              <image href={falsch[data.item_3]} height="120" width="328" />
+            </svg>
+          </li>,
+        ]
+        const shuffledItems = data.order.map(i => listItems[i])
+        return (
+          <>
+            <p>
+              Abbildung 2 stellt das Netz eines Tetraeders dar. Welches Netz
+              stellt ebenfalls das Netz eines Tetraeders dar? Entscheide
+              jeweils.
+            </p>
+            <ul>{shuffledItems}</ul>
+          </>
+        )
       },
       solution({ data }) {
         return <></>
