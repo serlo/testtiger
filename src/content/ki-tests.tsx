5b193bbf049313b2014115128d48d73b6536c4df
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-11-08 12:49:59 +0100

    add two more tests

--- src/content/ki-tests.tsx
+++ src/content/ki-tests.tsx
@@ -2,6 +2,19 @@ import { KITestEntry } from '@/data/types'
 
 export const KiTests: KITestEntry[] = [
   {
+    exerciseId: 17,
+    index: 'b',
+    input:
+      'm = (2 - 3) / (0 - (-2)) = -1 / 2 = -0,5 und b = 2. Die Gleichung lautet y = -0,5x + 2',
+    success: true,
+  },
+  {
+    exerciseId: 32,
+    index: 'c',
+    input: '1 und 24 sowie 2 und 12',
+    success: true,
+  },
+  {
     exerciseId: 54,
     index: 'd',
     input: '3/10',
