8cbca1a2dd5395798c39df1fac5d99b6b6c10b4e
alinegeissler <aline-geissler@web.de>
2024-11-12 10:15:30 +0100

    124

--- src/content/implementations/NRW-EESA/124-2023-1-5-Schätzen.tsx
+++ src/content/implementations/NRW-EESA/124-2023-1-5-Schätzen.tsx
@@ -1,6 +1,9 @@
 import { Exercise } from '@/data/types'
+import { getImageAndDescription } from '@/helper/get-image-and-description'
 
-interface DATA {}
+interface DATA {
+  task: number
+}
 
 export const exercise124: Exercise<DATA> = {
   title: 'Schätzen',
@@ -9,13 +12,62 @@ export const exercise124: Exercise<DATA> = {
   duration: 42,
   points: 42,
   generator(rng) {
-    return {}
+    const task = rng.randomIntBetween(1, 4)
+    return { task }
   },
+  originalData: { task: 4 },
   constraint({ data }) {
     return true
   },
   task({ data }) {
-    return <></>
+    return (
+      <>
+        <p>
+          Bestimme näherungsweise die Anzahl der{' '}
+          {data.task === 1 && 'Kaffeebohnen'}
+          {data.task === 2 && 'Bälle'}
+          {data.task === 3 && 'Beeren'}
+          {data.task === 4 && 'Regen'} auf dem Bild. Beschreibe, wie du
+          vorgegangen bist!
+        </p>
+        {data.task === 1 && (
+          <svg viewBox="0 0 1037 1469">
+            <image
+              href="public/content/NRW_EESA/124_Kaffeebohnen.jpeg"
+              height="1469"
+              width="1037"
+            />
+          </svg>
+        )}
+        {data.task === 2 && (
+          <svg viewBox="0 0 5234 3453">
+            <image
+              href="public/content/NRW_EESA/124_Bälle.jpg"
+              height="3453"
+              width="5234"
+            />
+          </svg>
+        )}
+        {data.task === 3 && (
+          <svg viewBox="0 0 3132 2072">
+            <image
+              href="public/content/NRW_EESA/124_Beeren.jpg"
+              height="2072"
+              width="3132"
+            />
+          </svg>
+        )}
+        {data.task === 4 && (
+          <svg viewBox="0 0 2448 3264">
+            <image
+              href="public/content/NRW_EESA/124_Regen.jpg"
+              height="3264"
+              width="2448"
+            />
+          </svg>
+        )}
+      </>
+    )
   },
   solution({ data }) {
     return <></>
