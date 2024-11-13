9e34de8407e8ae5538ac7b27cb7f0d93e5d228dd
alinegeissler <aline-geissler@web.de>
2024-11-13 17:07:35 +0100

    Update 125-2023-1-6-Mittelwerte und Durchschnitt.tsx

--- src/content/implementations/NRW-EESA/125-2023-1-6-Mittelwerte und Durchschnitt.tsx
+++ src/content/implementations/NRW-EESA/125-2023-1-6-Mittelwerte und Durchschnitt.tsx
@@ -2,9 +2,9 @@ import { Exercise } from '@/data/types'
 
 interface DATA {}
 
-export const exerciseXXX: Exercise<DATA> = {
-  title: 'NEU',
-  source: '',
+export const exercise125: Exercise<DATA> = {
+  title: 'Mittelwerte und Durchschnitt',
+  source: '2023 Teil 1 Aufgabe 6',
   useCalculator: false,
   duration: 42,
   generator(rng) {
