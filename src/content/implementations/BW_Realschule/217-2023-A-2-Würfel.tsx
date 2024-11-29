242d9690d5027e2b7401931942aa848d085a13bc
alinegeissler <aline-geissler@web.de>
2024-11-29 14:52:27 +0100

    Create 217-2023-A-2-Würfel.tsx

---
+++ src/content/implementations/BW_Realschule/217-2023-A-2-Würfel.tsx
@@ -0,0 +1,46 @@
+import { Exercise } from '@/data/types'
+
+interface DATA {}
+
+export const exercise217: Exercise<DATA> = {
+  title: 'Würfel',
+  source: '2023 Pflichtteil A - Aufgabe 2',
+  useCalculator: false,
+  duration: 42,
+  generator(rng) {
+    return {}
+  },
+  originalData: {},
+  constraint({ data }) {
+    return true
+  },
+  intro({ data }) {
+    return null
+  },
+  tasks: [
+    {
+      points: 42,
+      intro({ data }) {
+        return null
+      },
+      task({ data }) {
+        return <></>
+      },
+      solution({ data }) {
+        return <></>
+      },
+    },
+    {
+      points: 42,
+      intro({ data }) {
+        return null
+      },
+      task({ data }) {
+        return <></>
+      },
+      solution({ data }) {
+        return <></>
+      },
+    },
+  ],
+}
