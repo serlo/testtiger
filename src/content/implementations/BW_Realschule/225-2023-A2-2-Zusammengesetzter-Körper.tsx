92ce35d644fa29e90cde8147fa645924e88e7184
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-12-03 08:32:20 +0100

    New exercise 225

--- src/content/implementations/BW_Realschule/225-2023-A2-2-Zusammengesetzter-Körper.tsx
+++ src/content/implementations/BW_Realschule/225-2023-A2-2-Zusammengesetzter-Körper.tsx
@@ -1,5 +1,7 @@
 import { Exercise } from '@/data/types'
+import { buildEquation, buildInlineFrac } from '@/helper/math-builder'
 import { pp } from '@/helper/pretty-print'
+import { roundToDigits } from '@/helper/round-to-digits'
 
 interface DATA {
   s: number
@@ -52,6 +54,237 @@ export const exercise225: Exercise<DATA> = {
     )
   },
   solution({ data }) {
-    return <></>
+    const a = roundToDigits(
+      2 * Math.cos((data.alpha * 2 * Math.PI) / 360) * data.s,
+      2,
+    )
+    const hs = roundToDigits(Math.sqrt(data.s * data.s - (a / 2) * (a / 2)), 2)
+    const h = roundToDigits(Math.sqrt(hs * hs - (a / 2) * (a / 2)), 2)
+
+    return (
+      <>
+        <p>
+          <strong>Fläche des Dreiecks</strong>
+        </p>
+        <svg viewBox="0 0 328 220">
+          <image
+            href="/content/BW_Realschule/225_Körper2.jpg"
+            height="220"
+            width="328"
+          />
+        </svg>
+        <p>Berechne die Länge von a im rechtwinkligen Dreieck:</p>
+        <div>
+          <span style={{ fontSize: '0.8em' }}>
+            {buildEquation([
+              [
+                <>cos(α)</>,
+                <>=</>,
+                <>{buildInlineFrac(<>{buildInlineFrac('a', 2)}</>, <>s</>)}</>,
+              ],
+              [
+                <>cos({pp(data.alpha)}°)</>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(
+                    <>{buildInlineFrac('a', 2)}</>,
+                    <>{pp(data.s)} cm</>,
+                  )}
+                </>,
+                <>| · {pp(data.s)} cm</>,
+              ],
+              [
+                <>{buildInlineFrac('a', 2)}</>,
+                <>=</>,
+                <>
+                  cos({pp(data.alpha)}°) · {pp(data.s)} cm
+                </>,
+                <>| · 2</>,
+              ],
+              [
+                <>a</>,
+                <>=</>,
+                <>
+                  2 · cos({pp(data.alpha)}°) · {pp(data.s)} cm
+                </>,
+                <>| · 2</>,
+              ],
+              [<>a</>, <>≈</>, <>{pp(a)} cm</>],
+            ])}
+          </span>
+        </div>
+        <p>
+          Berechne die Höhe des schrägen Dreiecks mit dem Satz des Pythagoras:
+        </p>
+        <div>
+          <span style={{ fontSize: '0.7em' }}>
+            {buildEquation([
+              [
+                <>
+                  <span className="inline-block  scale-y-[2]">(</span>
+                  {buildInlineFrac('a', 2)}
+                  <span className="inline-block  scale-y-[2]">)</span>² + h
+                  <sub>s</sub>²
+                </>,
+                <>=</>,
+                <>s²</>,
+              ],
+              [
+                <>
+                  ({pp(a / 2)} cm)² + h<sub>s</sub>²
+                </>,
+                <>=</>,
+                <>({pp(data.s)} cm)²</>,
+                <>| − ({pp(a / 2)} cm)²</>,
+              ],
+              [
+                <>
+                  h<sub>s</sub>²
+                </>,
+                <>=</>,
+                <>
+                  ({pp(data.s)} cm)² − ({pp(a / 2)} cm)²
+                </>,
+                <>| √</>,
+              ],
+              [
+                <>
+                  h<sub>s</sub>
+                </>,
+                <>≈</>,
+                <>{pp(hs)} cm</>,
+              ],
+            ])}
+          </span>
+        </div>
+        <p>Damit lässt sich die Höhe h der Pyramide berechnen:</p>
+        <div>
+          <span style={{ fontSize: '0.7em' }}>
+            {buildEquation([
+              [
+                <>
+                  <span className="inline-block  scale-y-[2]">(</span>
+                  {buildInlineFrac('a', 2)}
+                  <span className="inline-block  scale-y-[2]">)</span>² + h²
+                </>,
+                <>=</>,
+                <>
+                  h<sub>s</sub>²
+                </>,
+              ],
+              [
+                <>({pp(a / 2)} cm)² + h</>,
+                <>=</>,
+                <>({pp(hs)} cm)²</>,
+                <>| − ({pp(a / 2)} cm)²</>,
+              ],
+              [
+                <>h²</>,
+                <>=</>,
+                <>
+                  ({pp(hs)} cm)² − ({pp(a / 2)} cm)²
+                </>,
+                <>| √</>,
+              ],
+              [<>h</>, <>≈</>, <>{pp(h)} cm</>],
+            ])}
+          </span>
+        </div>
+        <p>Bestimme damit die Fläche des grauen Dreiecks:</p>
+        {buildEquation([
+          [
+            <>
+              A<sub>1</sub>
+            </>,
+            <>=</>,
+            <>{buildInlineFrac(<>a · h</>, 2)}</>,
+          ],
+          [
+            <></>,
+            <>=</>,
+            <>
+              {buildInlineFrac(
+                <>
+                  {pp(a)} cm · {pp(h)} cm
+                </>,
+                2,
+              )}
+            </>,
+          ],
+          [
+            <></>,
+            <>≈</>,
+            <>
+              <strong>{pp(roundToDigits(a * h, 2))} cm²</strong>
+            </>,
+          ],
+        ])}
+        <p>
+          <strong>Fläche des Rechtecks</strong>
+        </p>
+        <p>Berechne die Höhe des quadratischen Prismas:</p>
+        {buildEquation([
+          [
+            <>
+              h<sub>Prisma</sub>
+            </>,
+            <>=</>,
+            <>
+              h<sub>ges</sub> − h
+            </>,
+          ],
+          [
+            <></>,
+            <>=</>,
+            <>
+              {pp(data.höhe)} cm − {pp(h)} cm
+            </>,
+          ],
+          [<></>, <>=</>, <>{pp(data.höhe - h)} cm</>],
+        ])}
+        <p>Bestimme damit die graue Rechtecksfläche:</p>
+        {buildEquation([
+          [
+            <>
+              A<sub>2</sub>
+            </>,
+            <>=</>,
+            <>
+              a · h<sub>Prisma</sub>
+            </>,
+          ],
+          [
+            <></>,
+            <>=</>,
+            <>
+              {pp(a)} cm · {pp(data.höhe - h)} cm
+            </>,
+          ],
+          [
+            <></>,
+            <>≈</>,
+            <>
+              <strong>{pp(roundToDigits(a * (data.höhe - h), 2))} cm²</strong>
+            </>,
+          ],
+        ])}
+        <p>
+          <strong>
+            Fläche A<sub>s</sub> berechnen
+          </strong>
+        </p>
+        <p>Insgesamt beträgt der Flächeninhalt:</p>
+        <p>
+          A<sub>s</sub> = {pp(roundToDigits(a * h, 2))} cm² +{' '}
+          {pp(roundToDigits(a * (data.höhe - h), 2))} cm² ={' '}
+          <strong>
+            {pp(
+              roundToDigits(a * h, 2) + roundToDigits(a * (data.höhe - h), 2),
+            )}{' '}
+            cm²
+          </strong>
+        </p>
+      </>
+    )
   },
 }
