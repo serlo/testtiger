8e0f2311bd52dc692d8488ceeff6ae451f30a694
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-12-05 11:58:20 +0100

    Exercise 247a done

--- src/content/implementations/BW_Realschule/247-2022-B-1-Figuren+Funktionen.tsx
+++ src/content/implementations/BW_Realschule/247-2022-B-1-Figuren+Funktionen.tsx
@@ -1,7 +1,16 @@
 import { Exercise } from '@/data/types'
-import { buildOverline } from '@/helper/math-builder'
+import {
+  buildEquation,
+  buildInlineFrac,
+  buildOverline,
+} from '@/helper/math-builder'
+import { pp } from '@/helper/pretty-print'
+import { roundToDigits } from '@/helper/round-to-digits'
 
-interface DATA {}
+interface DATA {
+  ab: number
+  af: number
+}
 
 export const exercise247: Exercise<DATA> = {
   title: 'Figuren + Funktionen',
@@ -9,11 +18,15 @@ export const exercise247: Exercise<DATA> = {
   useCalculator: true,
   duration: 42,
   generator(rng) {
-    return {}
+    return { ab: rng.randomIntBetween(8, 14), af: rng.randomIntBetween(10, 16) }
   },
-  originalData: {},
+  originalData: { ab: 14, af: 12 },
   constraint({ data }) {
-    return true
+    const ag = roundToDigits(
+      Math.sqrt(data.af * data.af - (data.ab / 2) * (data.ab / 2)),
+      2,
+    )
+    return ag + 2 < data.ab
   },
   intro({ data }) {
     return null
@@ -40,8 +53,8 @@ export const exercise247: Exercise<DATA> = {
             </svg>
             <p>Es gilt:</p>
             <p>
-              {buildOverline('AB')} = 10 cm<br></br>
-              {buildOverline('AF')} = 12 cm<br></br>
+              {buildOverline('AB')} = {data.ab} cm<br></br>
+              {buildOverline('AF')} = {data.af} cm<br></br>
               {buildOverline('AF')} = {buildOverline('BF')}
               <br></br>
               {buildOverline('EF')} = {buildOverline('DF')}
@@ -55,7 +68,210 @@ export const exercise247: Exercise<DATA> = {
         )
       },
       solution({ data }) {
-        return <></>
+        const ag = roundToDigits(
+          Math.sqrt(data.af * data.af - (data.ab / 2) * (data.ab / 2)),
+          2,
+        )
+        const ge = roundToDigits(data.ab - ag, 2)
+        const ae = ag - ge
+        const alpha = roundToDigits(
+          (360 * Math.atan(ge / (data.ab / 2))) / (2 * Math.PI),
+          2,
+        )
+        const alphaepsilon = roundToDigits(
+          (360 * Math.atan(ag / (data.ab / 2))) / (2 * Math.PI),
+          2,
+        )
+        return (
+          <>
+            <p>
+              <strong>Fläche von AFE</strong>
+            </p>
+            <p>
+              Berechne zuerst die Höhe {buildOverline('GF')} und die Länge{' '}
+              {buildOverline('AE')}.
+            </p>
+            <svg viewBox="0 0 328 210">
+              <image
+                href="/content/BW_Realschule/247_Figur2.jpg"
+                height="210"
+                width="328"
+              />
+            </svg>
+            <p>
+              Die Höhe {buildOverline('GF')} entspricht genau der halben Strecke
+              AB:
+            </p>
+            <p>
+              {buildOverline('GF')} = {buildOverline('AB')} : 2 = {data.ab} cm :
+              2 = {pp(data.ab / 2)} cm
+            </p>
+            <p>
+              Im rechtwinkligen Dreieck AFG lässt sich die Länge{' '}
+              {buildOverline('AG')} mit dem Satz des Pythagoras berechnen:
+            </p>
+            <div>
+              <span style={{ fontSize: '0.8em' }}>
+                {buildEquation([
+                  [
+                    <>{buildOverline('AF')}²</>,
+                    <>=</>,
+                    <>
+                      {buildOverline('AG')}² + {buildOverline('GF')}²
+                    </>,
+                  ],
+                  [
+                    <>({pp(data.af)} cm)²</>,
+                    <>=</>,
+                    <>
+                      {buildOverline('AG')}² + ({pp(data.ab / 2)} cm)²
+                    </>,
+                    <>| − ({pp(data.ab / 2)} cm)²</>,
+                  ],
+                  [
+                    <>{buildOverline('AG')}²</>,
+                    <>=</>,
+                    <>
+                      ({pp(data.af)} cm)² − ({pp(data.ab / 2)} cm)²
+                    </>,
+                    <>| √</>,
+                  ],
+                  [<>{buildOverline('AG')}</>, <>≈</>, <>{pp(ag)} cm</>],
+                ])}
+              </span>
+            </div>
+            <p>Berechne damit {buildOverline('DG')}:</p>
+            {buildEquation([
+              [
+                <>{buildOverline('DG')}</>,
+                <>=</>,
+                <>
+                  {buildOverline('AD')} − {buildOverline('AG')}
+                </>,
+              ],
+              [
+                <></>,
+                <>=</>,
+                <>
+                  {pp(data.ab)} cm − {pp(ag)} cm
+                </>,
+              ],
+              [<></>, <>=</>, <>{pp(ge)} cm</>],
+            ])}
+
+            <p>Damit ist {buildOverline('AE')}:</p>
+            {buildEquation([
+              [
+                <>{buildOverline('AE')}</>,
+                <>=</>,
+                <>
+                  {buildOverline('AG')} − {buildOverline('GE')}
+                </>,
+              ],
+              [
+                <></>,
+                <>=</>,
+                <>
+                  {pp(ag)} cm − {pp(ge)} cm
+                </>,
+              ],
+              [<></>, <>=</>, <>{pp(ae)} cm</>],
+            ])}
+            <p>Berechne die Dreiecksfläche:</p>
+            {buildEquation([
+              [
+                <>A</>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(
+                    <>
+                      {buildOverline('AE')} · {buildOverline('GF')}
+                    </>,
+                    <>2</>,
+                  )}
+                </>,
+              ],
+              [
+                <></>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(
+                    <>
+                      {pp(ae)} cm · {pp(data.ab / 2)} cm
+                    </>,
+                    <>2</>,
+                  )}
+                </>,
+              ],
+              [
+                <></>,
+                <>=</>,
+                <>
+                  <strong>{pp((ae * (data.ab / 2)) / 2)} cm²</strong>
+                </>,
+              ],
+            ])}
+            <p>
+              <strong>ε berechnen</strong>
+            </p>
+            <p>Bestimme die Größe von α im rechtwinkligen Dreieck EFG:</p>
+            {buildEquation([
+              [
+                <>tan(α)</>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(
+                    <>{buildOverline('GE')}</>,
+                    <>{buildOverline('GF')}</>,
+                  )}
+                </>,
+              ],
+              [
+                <>tan(α)</>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(<>{pp(ge)} cm</>, <>{pp(data.ab / 2)} cm</>)}
+                </>,
+                <>
+                  | tan<sup>-1</sup>()
+                </>,
+              ],
+              [<>α</>, <>≈</>, <>{pp(alpha)}°</>],
+            ])}
+            <p>
+              Mit der gleichen Rechnung kann im rechtwinkligen Dreieck AFG der
+              zusammengesetzte Winkel α + ε berechnet werden:
+            </p>
+            {buildEquation([
+              [
+                <>tan(α + ε)</>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(
+                    <>{buildOverline('AG')}</>,
+                    <>{buildOverline('GF')}</>,
+                  )}
+                </>,
+              ],
+              [
+                <>tan(α + ε)</>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(<>{pp(ag)} cm</>, <>{pp(data.ab / 2)} cm</>)}
+                </>,
+                <>
+                  | tan<sup>-1</sup>()
+                </>,
+              ],
+              [<>α + ε</>, <>≈</>, <>{pp(alphaepsilon)}°</>],
+            ])}
+            <p>Berechne damit ε:</p>
+            <p>
+              ε = {pp(alphaepsilon)}° − {pp(alpha)}° ={' '}
+              <strong>{pp(alphaepsilon - alpha)}°</strong>
+            </p>
+          </>
+        )
       },
     },
     {
