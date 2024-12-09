75f4530e0ce76df665469afbb79ee2c839f9342b
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-12-09 17:21:40 +0100

    draw path

--- src/components/learning-path/LearningPath.tsx
+++ src/components/learning-path/LearningPath.tsx
@@ -25,3 +25,38 @@ export function LearningPath() {
     </IonPage>
   )
 }
+
+/**
+ * Ein paar Notizen: Ein Kernelement des Lernpfads ist die Speicherung des Profils. Um das möglich zu machen, braucht es
+ * ein paar Datenstrukturen.
+ *
+ * Reicht es aus, nur die Liste der gelösten Teilaufgaben vorzuhalten? Ja, das würde gehen.
+ *
+ * Ich muss daraus viele abgeleitete Datenpakete generieren.
+ *
+ * Teil
+ *  - Stern-Abschnitt
+ *    - Lektion (Punkt im Pfad)
+ *      - Schritt (Aufgaben innerhalb des Pfads)
+ *
+ * Jede Lektion braucht die Anzahl der Teilaufgaben insgesamt und die Anzahl der gelösten Teilaufgaben davon
+ *
+ * Ich muss wissen, welche Sterne freigeschaltet sind - das ist eine zentrale Info
+ *
+ * Ich sehe das potenzielle Problem, dass sich dieser Code über die drei Abschnitte duplizieren könnte - da bräuchte es vielleicht etwas Abstrkation.
+ *
+ * Zeichentechnik für den Pfad festlegen?
+ * Ich denke, ich kann ein "hop here, hop there" Entwicklungsmodus anstreben. Es gibt verschiedene Baustellen und ich dazwischen hin und her springen.
+ *
+ * Also Baustellen sind Frontend / Datastruktur / Step-by-Step-Visualisation
+ *
+ * Alle Elemente drehen sich um den Lernpfad. Das ist eine praktische Einsicht. Wenn ich den Lernpfad gut baue, dann wird der Rest der Erfahrung auch leicht umsetzbar sein.
+ *
+ * Wie verwalte ich transient states? Es wird immer irgendwelche Aktivitäten geben, Interaktionen verursachen werden ...
+ *
+ * Ich schaffe es ja nicht einmal, die Scroll-Position gut zu verwalten, rip
+ *
+ * Ok, egal, ich versuche jetzt mal den Pfad zu zeichnen
+ *
+ * Eigentlich wäre es nicht schlecht für die einzelnen Dots jeweils eine ID o.ä. zu haben um besser darauf zugreifen zu können
+ */
--- src/components/learning-path/LearningPathMap.tsx
+++ src/components/learning-path/LearningPathMap.tsx
@@ -1,5 +1,75 @@
+import { navigationData } from '@/content/navigations'
+import { PlayerProfileStore } from '../../../store/player-profile-store'
+import { Lesson } from '@/data/types'
+
 export function LearningPathMap() {
+  const exam = PlayerProfileStore.useState(s => s.currentExam)
+
+  const path = navigationData[exam].path
+
+  const elements: { source: Lesson }[] = []
+  const lines: { start: Lesson; end: Lesson }[] = []
+
+  for (const part of path) {
+    let prev: Lesson | null = null
+    for (const lesson of part.lessons) {
+      if (lesson.position) {
+        elements.push({ source: lesson })
+        if (prev && prev.position) {
+          lines.push({ start: prev, end: lesson })
+        }
+      }
+      prev = lesson
+    }
+  }
   return (
-    <div className="h-[20000px] bg-gradient-to-t from-green-300 to-blue-300"></div>
+    <div className="bg-gradient-to-t from-green-300 to-blue-300 relative">
+      <svg viewBox="0 0 375 10000">
+        {lines.map((l, i) => (
+          <line
+            key={i}
+            x1={l.start.position!.x}
+            y1={10000 - l.start.position!.y}
+            x2={l.end.position!.x}
+            y2={10000 - l.end.position!.y}
+            stroke="gray"
+            strokeWidth={5}
+          ></line>
+        ))}
+        {elements.map((el, i) => (
+          <circle
+            key={i}
+            cx={el.source.position!.x}
+            cy={10000 - el.source.position!.y}
+            r={25}
+            fill={
+              el.source.type == 'new-skill'
+                ? 'rebeccapurple'
+                : el.source.type == 'challenge'
+                  ? 'yellow'
+                  : 'gray'
+            }
+            className="cursor-pointer"
+          ></circle>
+        ))}
+        {elements.map((el, i) => {
+          if (el.source.type == 'challenge') {
+            return (
+              <text
+                key={i}
+                x={el.source.position!.x}
+                y={10000 - el.source.position!.y + 5}
+                textAnchor="middle"
+                fontSize={18}
+                className="pointer-events-none"
+              >
+                {el.source.title.replace('Challenge', '').trim()}
+              </text>
+            )
+          }
+          return null
+        })}
+      </svg>
+    </div>
   )
 }
