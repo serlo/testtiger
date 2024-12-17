404586e05d6d45be645e535ff3e978c02cab7de5
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-12-17 14:16:58 +0100

    264 a) done

--- src/content/implementations/BW_Realschule/264-2021-B-1-Figur+Funktionen.tsx
+++ src/content/implementations/BW_Realschule/264-2021-B-1-Figur+Funktionen.tsx
@@ -1,7 +1,18 @@
 import { Exercise } from '@/data/types'
-import { buildOverline } from '@/helper/math-builder'
+import {
+  buildEquation,
+  buildInlineFrac,
+  buildOverline,
+} from '@/helper/math-builder'
+import { pp } from '@/helper/pretty-print'
+import { roundToDigits } from '@/helper/round-to-digits'
+import { DO_NOT_USE_OR_YOU_WILL_BE_FIRED_EXPERIMENTAL_REACT_NODES } from 'react'
 
-interface DATA {}
+interface DATA {
+  ab: number
+  alpha: number
+  ce: number
+}
 
 export const exercise264: Exercise<DATA> = {
   title: 'Figur + Funktionen',
@@ -9,9 +20,13 @@ export const exercise264: Exercise<DATA> = {
   useCalculator: true,
   duration: 42,
   generator(rng) {
-    return {}
+    return {
+      ab: rng.randomIntBetween(100, 150) / 10,
+      alpha: rng.randomIntBetween(50, 75),
+      ce: rng.randomIntBetween(60, 90) / 10,
+    }
   },
-  originalData: {},
+  originalData: { ab: 13.2, alpha: 55, ce: 8 },
   constraint({ data }) {
     return true
   },
@@ -40,8 +55,9 @@ export const exercise264: Exercise<DATA> = {
             </svg>
             <p>
               Es gilt: <br></br>
-              {buildOverline('AB')} = 13,2 cm <br></br>α = 55,0°<br></br>
-              {buildOverline('CE')} = 8,0 cm<br></br>
+              {buildOverline('AB')} = {pp(data.ab)} cm <br></br>α ={' '}
+              {pp(data.alpha)}°<br></br>
+              {buildOverline('CE')} = {pp(data.ce)} cm<br></br>
               {buildOverline('AE')} = {buildOverline('DE')}
             </p>
             <ul>
@@ -52,7 +68,202 @@ export const exercise264: Exercise<DATA> = {
         )
       },
       solution({ data }) {
-        return <></>
+        const ac = roundToDigits(
+          data.ab / Math.cos((2 * Math.PI * data.alpha) / 360),
+          2,
+        )
+        const df = ac - data.ce - data.ce
+        const bf = roundToDigits(
+          df * Math.sin((2 * Math.PI * data.alpha) / 360),
+          2,
+        )
+        return (
+          <>
+            <p>
+              <strong>Länge von {buildOverline('DF')}</strong>
+            </p>
+            <p>
+              Berechne die Länge {buildOverline('AC')} im rechtwinkligen Dreieck
+              ABC:
+            </p>
+            {buildEquation([
+              [
+                <>cos(α)</>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(
+                    <>{buildOverline('AB')}</>,
+                    <>{buildOverline('AC')}</>,
+                  )}
+                </>,
+              ],
+              [
+                <>cos({pp(data.alpha)}°)</>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(
+                    <>{pp(data.ab)} cm</>,
+                    <>{buildOverline('AC')}</>,
+                  )}
+                </>,
+                <>| · {buildOverline('AC')}</>,
+              ],
+              [
+                <>
+                  {buildOverline('AC')} · cos({pp(data.alpha)}°)
+                </>,
+                <>=</>,
+                <>{pp(data.ab)} cm</>,
+                <>| : cos({pp(data.alpha)}°)</>,
+              ],
+              [
+                <>{buildOverline('AC')}</>,
+                <>=</>,
+                <>
+                  {buildInlineFrac(
+                    <>{pp(data.ab)} cm</>,
+                    <>cos({pp(data.alpha)}°)</>,
+                  )}
+                </>,
+              ],
+              [
+                <>{buildOverline('AC')}</>,
+                <>≈</>,
+                <>
+                  <strong>{pp(ac)} cm</strong>
+                </>,
+              ],
+            ])}
+            <p>Bestimme die Länge {buildOverline('AE')}:</p>
+            <p>
+              {buildOverline('AE')} = {buildOverline('AC')} −{' '}
+              {buildOverline('CE')} = <strong>{pp(ac - data.ce)} cm</strong>
+            </p>
+            <p>
+              Damit ist auch: {buildOverline('DE')} ={' '}
+              <strong>{pp(ac - data.ce)} cm</strong>
+            </p>
+
+            <p>
+              Um die Länge {buildOverline('DF')} über {buildOverline('EF')} zu
+              bestimmen, muss begründet werden, dass das Dreieck CEF
+              gleichschenklig ist:
+            </p>
+            <svg viewBox="0 0 328 190">
+              <image
+                href="/content/BW_Realschule/264_Figur2.jpg"
+                height="190"
+                width="328"
+              />
+            </svg>
+            <ul>
+              <li>
+                β = 180° − 2 · {pp(data.alpha)}° = {pp(180 - 2 * data.alpha)}°
+              </li>
+              <li>
+                γ = 180° − {pp(180 - 2 * data.alpha)}° = {pp(2 * data.alpha)}°
+              </li>
+              <li>δ = 180° − 90° − α = {pp(180 - 90 - data.alpha)}°</li>
+              <li>
+                ε = 180° − {pp(2 * data.alpha)}° − {pp(180 - 90 - data.alpha)}°
+                = {pp(180 - 90 - data.alpha)}°
+              </li>
+            </ul>
+            <p>
+              Da die Winkel δ und ε gleich sind, ist das Dreieck
+              gleichschenklig. Das bedeutet:<br></br> {buildOverline('EF')} ={' '}
+              {buildOverline('CE')} = <strong>{pp(data.ce)} cm</strong>
+            </p>
+            <p>Berechne damit {buildOverline('DF')}:</p>
+            <p>
+              {buildOverline('DF')} = {buildOverline('DE')} −{' '}
+              {buildOverline('EF')} ={' '}
+              <strong>{pp(ac - data.ce - data.ce)} cm</strong>
+            </p>
+            <p>
+              <strong>Umfang von ABFE</strong>
+            </p>
+            <p>
+              Berechne die Länge {buildOverline('BF')} im rechtwinkligen Dreieck
+              BDF.
+            </p>
+            <div>
+              <span style={{ fontSize: '0.9em' }}>
+                {buildEquation([
+                  [
+                    <>sin(α)</>,
+                    <>=</>,
+                    <>
+                      {buildInlineFrac(
+                        <>{buildOverline('BF')}</>,
+                        <>{buildOverline('DF')}</>,
+                      )}
+                    </>,
+                  ],
+                  [
+                    <>sin({pp(data.alpha)}°)</>,
+                    <>=</>,
+                    <>
+                      {buildInlineFrac(
+                        <>{buildOverline('BF')}</>,
+                        <>{pp(df)} cm</>,
+                      )}
+                    </>,
+                    <>| · {pp(df)} cm</>,
+                  ],
+                  [
+                    <>{buildOverline('BF')}</>,
+                    <>=</>,
+                    <>
+                      sin({pp(data.alpha)}°) · {pp(df)} cm
+                    </>,
+                  ],
+                  [
+                    <>{buildOverline('BF')}</>,
+                    <>≈</>,
+                    <>
+                      <strong>{pp(bf)} cm</strong>
+                    </>,
+                  ],
+                ])}
+              </span>
+            </div>
+            <p>
+              Berechne aus den einzelnen Seitenlängen den Umfang des Vierecks:
+            </p>
+            <div>
+              <span style={{ fontSize: '0.9em' }}>
+                {buildEquation([
+                  [
+                    <>U</>,
+                    <>=</>,
+                    <>
+                      {buildOverline('BF')} + {buildOverline('EF')} +{' '}
+                      {buildOverline('AE')} + {buildOverline('AB')}
+                    </>,
+                  ],
+                  [
+                    <></>,
+                    <>=</>,
+                    <>
+                      {pp(bf)} cm + {pp(data.ce)} cm + {pp(ac - data.ce)} cm +{' '}
+                      {pp(data.ab)} cm
+                    </>,
+                  ],
+                  [
+                    <></>,
+                    <>=</>,
+                    <>
+                      <strong>
+                        {pp(bf + data.ce + ac - data.ce + data.ab)} cm
+                      </strong>
+                    </>,
+                  ],
+                ])}
+              </span>
+            </div>
+          </>
+        )
       },
     },
     {
