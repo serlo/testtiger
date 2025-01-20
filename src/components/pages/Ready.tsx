06745a7b6e4a28f0852a64086f22fd74223895a4
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-01-20 14:49:49 +0100

    prompt to install (not dynamic yet)

--- src/components/pages/Ready.tsx
+++ src/components/pages/Ready.tsx
@@ -4,16 +4,28 @@ export function Ready() {
   return (
     <IonPage className="sm:max-w-[375px] mx-auto">
       <IonContent className="ion-padding">
-        <div className="text-center text-3xl mt-16">Bereit!</div>
-        <div className="text-5xl text-center mt-16 mb-16">🎓</div>
-        <div className="text-center text-gray-600 mt-6 max-w-[360px] mx-auto">
-          Kein Stress - du wirst hier zum TestTiger 🙌
+        <div className="text-center text-3xl mt-16">
+          Füge den TestTiger zum Startbildschirm:
+        </div>
+        <div className="mt-12 mx-4">
+          Klicke auf das Browser-Menü (oben rechts oder unten in der Mitte)
+          <br />
+          <br />
+          und wähle
+          <br />
+          <br />
+          &quot;<strong>Zum Startbildschirm hinzufügen</strong>&quot; oder
+          &quot;<strong>Zum Home Bildschirm</strong>&quot;
+          <br />
+          <br />
+          <br />
+          Damit kannst du schnell auf die App zugreifen.
         </div>
       </IonContent>
       <IonFooter>
         <div className="py-3 text-center bg-white">
           <p className="mt-6 flex justify-around">
-            <IonButton routerLink="/app/home">Los gehts!</IonButton>
+            <IonButton routerLink="/app/home">Erledigt!</IonButton>
           </p>
         </div>
       </IonFooter>
