6e7095e432d7cb120fcd6a911a691125e3e6f49f
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-01-22 15:20:24 +0100

    change font to lato

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,14 @@ import '@ionic/react/css/core.css'
 
 import '../../styles/global.css'
 
-import { Inter } from 'next/font/google'
+import { Lato } from 'next/font/google'
+import clsx from 'clsx'
 
-const inter = Inter({
+const lato = Lato({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-inter',
+  weight: ['400', '700'],
+  variable: '--font-lato',
 })
 
 export const metadata: Metadata = {
@@ -25,7 +27,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="de" suppressHydrationWarning={true} className={inter.variable}>
+    <html
+      lang="de"
+      suppressHydrationWarning={true}
+      className={clsx(lato.variable)}
+    >
       <body>{children}</body>
     </html>
   )
