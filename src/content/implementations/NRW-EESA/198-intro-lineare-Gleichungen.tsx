97051efa4c014c77db9d3f633d84ba24dcc33181
alinegeissler <aline-geissler@web.de>
2025-01-29 08:39:44 +0100

    Update 198-intro-lineare-Gleichungen.tsx

--- src/content/implementations/NRW-EESA/198-intro-lineare-Gleichungen.tsx
+++ src/content/implementations/NRW-EESA/198-intro-lineare-Gleichungen.tsx
@@ -2,10 +2,13 @@ import { Exercise } from '@/data/types'
 import { buildEquation } from '@/helper/math-builder'
 
 interface DATA {
+  a: number
+  b: number
+  x_sol: number
+
   subtrahend: number
   faktor: number
   x_input: number
-  x_sol: number
 }
 
 export const exercise198: Exercise<DATA> = {
@@ -16,10 +19,13 @@ export const exercise198: Exercise<DATA> = {
   points: 42,
   generator(rng) {
     return {
+      a: rng.randomIntBetween(2, 5),
+      b: rng.randomIntBetween(6, 15),
+      x_sol: rng.randomIntBetween(2, 7),
+
       subtrahend: rng.randomIntBetween(6, 15),
       faktor: rng.randomIntBetween(2, 5),
       x_input: rng.randomIntBetween(3, 8),
-      x_sol: rng.randomIntBetween(2, 7),
     }
   },
 
@@ -27,57 +33,53 @@ export const exercise198: Exercise<DATA> = {
     return true
   },
   task({ data }) {
-    const rechts = (data.x_sol - data.subtrahend) * data.faktor
+    const rechts = data.a * data.x_sol + data.b
     return (
       <>
         <p>
           <b>Starte mit einer Aufgabe zum Aufwärmen:</b>
         </p>
-        <p>
-          Luisa und Paul rechnen beide die selbe Aufgabe: (x - {data.subtrahend}
-          ) · {data.faktor} = ... Sie wollen den Wert für x finden. Sie notieren
-          jeweils ihren Rechenweg. Welcher Rechenweg ist richtig?{' '}
-        </p>
-        <p>Luisas Rechenweg:</p>
+        <p>Löse die Gleichung:</p>
         {buildEquation([
           [
             <>
-              (x - {data.subtrahend}) · {data.faktor}
+              {data.a} x + {data.b}
             </>,
             <>=</>,
             <>{rechts}</>,
-            <>| : {data.faktor}</>,
           ],
-          [
-            <>x - {data.subtrahend}</>,
-            <>=</>,
-            <>{rechts / data.faktor}</>,
-            <>| + {data.subtrahend}</>,
-          ],
-          [<>x</>, <>=</>, <>{rechts / data.faktor + data.subtrahend}</>],
-        ])}
-        <p>Pauls Rechenweg:</p>
-        {buildEquation([
-          [
-            <>
-              (x - {data.subtrahend}) · {data.faktor}
-            </>,
-            <>=</>,
-            <>{rechts}</>,
-            <>| : {data.faktor}</>,
-          ],
-          [
-            <>x - {data.subtrahend}</>,
-            <>=</>,
-            <>{rechts}</>,
-            <>| + {data.subtrahend}</>,
-          ],
-          [<>x</>, <>=</>, <>{rechts + data.subtrahend}</>],
         ])}
       </>
     )
   },
   solution({ data }) {
-    return <></>
+    const rechts = data.a * data.x_sol + data.b
+    return (
+      <>
+        <p>Löse die Gleichung nach x auf:</p>
+        {buildEquation([
+          [
+            <>
+              {data.a} x + {data.b}
+            </>,
+            <>=</>,
+            <>{rechts}</>,
+            <>| - {data.b}</>,
+          ],
+          [<>{data.a} x</>, <>=</>, <>{rechts - data.b}</>, <>| : {data.a}</>],
+          [
+            <>
+              <b>x</b>
+            </>,
+            <>
+              <b>=</b>
+            </>,
+            <>
+              <b>{(rechts - data.b) / data.a}</b>
+            </>,
+          ],
+        ])}
+      </>
+    )
   },
 }
