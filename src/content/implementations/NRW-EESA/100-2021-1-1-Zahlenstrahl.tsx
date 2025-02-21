9dec6258fbcd2eac40fe76faff311b1c4817c41e
Alexandra-Wolfram <164320968+Alexandra-Wolfram@users.noreply.github.com>
2025-02-21 14:09:24 +0100

    Update 100-2021-1-1-Zahlenstrahl.tsx

--- src/content/implementations/NRW-EESA/100-2021-1-1-Zahlenstrahl.tsx
+++ src/content/implementations/NRW-EESA/100-2021-1-1-Zahlenstrahl.tsx
@@ -1,4 +1,5 @@
 import { Exercise } from '@/data/types'
+import { Color1 } from '@/helper/colors'
 import { pp, ppFrac } from '@/helper/pretty-print'
 
 interface DATA {
@@ -59,144 +60,380 @@ export const exercise100: Exercise<DATA> = {
     return (
       <>
         <p>
-          Schau dir den Abstand zwischen zwei Strichen am Zahlenstrahl an.
-          Zwischen 0 und 1 sind es 5 Striche. Der Abstand zwischen zwei Strichen
-          ist also immer 1 : 5 = 0,2.
+          Schau dir die Abstände am Zahlenstrahl an:
+          <br></br>
+          Zwischen 0 und 1 gibt es 5 Abschnitte. Der Abstand zwischen zwei
+          Strichen ist also immer 1 : 5 = 0,2.
+          <svg
+            width="350"
+            height="50"
+            viewBox="0 0 350 50"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <defs>
+              <marker
+                id="arrow"
+                markerWidth="10"
+                markerHeight="10"
+                refX="3"
+                refY="3"
+                orient="auto"
+                markerUnits="strokeWidth"
+              >
+                <path d="M0,0 L0,6 L6,3 Z" fill="black" />
+              </marker>
+            </defs>
+            <line
+              x1="20"
+              y1="20"
+              x2="320"
+              y2="20"
+              stroke="black"
+              stroke-width="1"
+              marker-end="url(#arrow)"
+            />
+            <line
+              x1="30"
+              y1="12"
+              x2="30"
+              y2="28"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="55.5"
+              y1="16"
+              x2="55.5"
+              y2="24"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="80.9"
+              y1="16"
+              x2="80.9"
+              y2="24"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="106.4"
+              y1="16"
+              x2="106.4"
+              y2="24"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="131.8"
+              y1="16"
+              x2="131.8"
+              y2="24"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="157.3"
+              y1="12"
+              x2="157.3"
+              y2="28"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="182.7"
+              y1="16"
+              x2="182.7"
+              y2="24"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="208.2"
+              y1="16"
+              x2="208.2"
+              y2="24"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="233.6"
+              y1="16"
+              x2="233.6"
+              y2="24"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="259.1"
+              y1="16"
+              x2="259.1"
+              y2="24"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="284.6"
+              y1="12"
+              x2="284.6"
+              y2="28"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="310"
+              y1="16"
+              x2="310"
+              y2="24"
+              stroke="black"
+              stroke-width="1"
+            />
+            <text x="30" y="40" text-anchor="middle" font-size="12">
+              0
+            </text>
+            <text x="157.3" y="40" text-anchor="middle" font-size="12">
+              1
+            </text>
+            <text x="284.6" y="40" text-anchor="middle" font-size="12">
+              2
+            </text>
+            <text x="55.5" y="38" text-anchor="middle" font-size="10">
+              0,2
+            </text>
+            <text x="80.9" y="38" text-anchor="middle" font-size="10">
+              0,4
+            </text>
+            <text x="106.4" y="38" text-anchor="middle" font-size="10">
+              0,6
+            </text>
+            <text x="131.8" y="38" text-anchor="middle" font-size="10">
+              0,8
+            </text>
+          </svg>
         </p>
-        <svg
-          width="300"
-          height="50"
-          viewBox="0 0 300 50"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs>
-            <marker
-              id="arrow"
-              markerWidth="10"
-              markerHeight="10"
-              refX="3"
-              refY="3"
-              orient="auto"
-              markerUnits="strokeWidth"
+        <p>
+          Wenn es dir hilft, kannst du die Brüche in Dezimalzahlen umwandeln:
+          <br></br>
+          <Color1>
+            {ppFrac([data.b, 10])}= {pp(data.b / 10)}
+          </Color1>{' '}
+          und{' '}
+          <Color1>
+            {ppFrac(data.c)} = {pp(data.c)}
+          </Color1>
+        </p>
+        <p>
+          <b>Markiere die Zahlen auf dem Zahlenstrahl:</b>
+          <svg
+            width="350"
+            height="60"
+            viewBox="0 0 350 60"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <defs>
+              <marker
+                id="arrow"
+                markerWidth="10"
+                markerHeight="10"
+                refX="3"
+                refY="3"
+                orient="auto"
+                markerUnits="strokeWidth"
+              >
+                <path d="M0,0 L0,6 L6,3 Z" fill="black" />
+              </marker>
+            </defs>
+            <line
+              x1="20"
+              y1="30"
+              x2="320"
+              y2="30"
+              stroke="black"
+              stroke-width="1"
+              marker-end="url(#arrow)"
+            />
+            <line
+              x1="30"
+              y1="22"
+              x2="30"
+              y2="38"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="55.5"
+              y1="26"
+              x2="55.5"
+              y2="34"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="80.9"
+              y1="26"
+              x2="80.9"
+              y2="34"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="106.4"
+              y1="26"
+              x2="106.4"
+              y2="34"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="131.8"
+              y1="26"
+              x2="131.8"
+              y2="34"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="157.3"
+              y1="22"
+              x2="157.3"
+              y2="38"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="182.7"
+              y1="26"
+              x2="182.7"
+              y2="34"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="208.2"
+              y1="26"
+              x2="208.2"
+              y2="34"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="233.6"
+              y1="26"
+              x2="233.6"
+              y2="34"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="259.1"
+              y1="26"
+              x2="259.1"
+              y2="34"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="284.6"
+              y1="22"
+              x2="284.6"
+              y2="38"
+              stroke="black"
+              stroke-width="1"
+            />
+            <line
+              x1="310"
+              y1="26"
+              x2="310"
+              y2="34"
+              stroke="black"
+              stroke-width="1"
+            />
+            <text x="30" y="50" text-anchor="middle" font-size="12">
+              0
+            </text>
+            <text x="157.3" y="50" text-anchor="middle" font-size="12">
+              1
+            </text>
+            <text x="284.6" y="50" text-anchor="middle" font-size="12">
+              2
+            </text>
+            <text
+              x={30 + 127.3 * data.a}
+              y="34"
+              text-anchor="middle"
+              fill="blue"
+              font-size="15"
             >
-              <path d="M0,0 L0,6 L6,3 z" fill="#000" />
-            </marker>
-          </defs>
+              x
+            </text>
+            <text
+              x={30 + 127.3 * data.a}
+              y="20"
+              text-anchor="middle"
+              fill="blue"
+              font-size="12"
+            >
+              {pp(data.a)}
+            </text>
+            <text
+              x={30 + 127.3 * data.c}
+              y="34"
+              text-anchor="middle"
+              fill="blue"
+              font-size="15"
+            >
+              x
+            </text>
 
-          <line
-            x1="20"
-            y1="20"
-            x2="280"
-            y2="20"
-            stroke="black"
-            stroke-width="2"
-            marker-end="url(#arrow)"
-          />
+            <text
+              x={30 + 127.3 * data.c}
+              y="46"
+              text-anchor="middle"
+              fill="blue"
+              font-size="12"
+            >
+              {pp(data.c)}
+            </text>
 
-          <line x1="20" y1="18" x2="20" y2="22" stroke="black" />
-          <line x1="52.5" y1="18" x2="52.5" y2="22" stroke="black" />
-          <line x1="85" y1="18" x2="85" y2="22" stroke="black" />
-          <line x1="117.5" y1="18" x2="117.5" y2="22" stroke="black" />
-          <line x1="150" y1="18" x2="150" y2="22" stroke="black" />
-          <line x1="182.5" y1="18" x2="182.5" y2="22" stroke="black" />
-          <line x1="215" y1="18" x2="215" y2="22" stroke="black" />
-          <line x1="247.5" y1="18" x2="247.5" y2="22" stroke="black" />
-          <line x1="280" y1="18" x2="280" y2="22" stroke="black" />
+            <text
+              x={30 + 127.3 * data.d}
+              y="34"
+              text-anchor="middle"
+              fill="blue"
+              font-size="15"
+            >
+              x
+            </text>
+            <text
+              x={30 + 127.3 * data.d}
+              y="20"
+              text-anchor="middle"
+              fill="blue"
+              font-size="12"
+            >
+              {pp(data.d)}
+            </text>
+            <text
+              x={30 + 127.3 * (data.b / 10)}
+              y="34"
+              text-anchor="middle"
+              fill="blue"
+              font-size="15"
+            >
+              x
+            </text>
 
-          <text x="20" y="35" text-anchor="middle" font-size="12">
-            0
-          </text>
-          <text x="150" y="35" text-anchor="middle" font-size="12">
-            1
-          </text>
-          <text x="280" y="35" text-anchor="middle" font-size="12">
-            2
-          </text>
-        </svg>
-        <p>Wandle zuerst die Brüche in Dezimalzahlen um:</p>
-        <p>
-          {ppFrac([data.b, 10])} = {pp(data.b / 10)}
+            <text
+              x={30 + 127.3 * (data.b / 10)}
+              y="20"
+              text-anchor="middle"
+              fill="blue"
+              font-size="12"
+            >
+              {pp(data.b / 10)}
+            </text>
+          </svg>
         </p>
-        <p>
-          {ppFrac(data.c)} = {pp(data.c)}
-        </p>
-        <p>
-          Markiere nun die Zahlen auf dem Zahlenstrahl. Der Abstand zwischen
-          zwei Strichen ist dabei immer 0,2.
-        </p>
-        <svg viewBox="0 0 328 110">
-          <image
-            href="/content/NRW_EESA/100_Zahlenstrahl.png"
-            height="110"
-            width="328"
-          />
-          <text
-            x={toX(data.a)}
-            y={56}
-            fontSize={15}
-            textAnchor="middle"
-            stroke="blue"
-          >
-            ×
-          </text>
-          <text
-            x={toX(data.a)}
-            y={73}
-            fontSize={14}
-            textAnchor="middle"
-            stroke="blue"
-            strokeWidth={0.1}
-          >
-            {pp(data.a)}
-          </text>
-          <text
-            x={toX(data.b / 10)}
-            y={56}
-            fontSize={15}
-            textAnchor="middle"
-            stroke="blue"
-          >
-            ×
-          </text>
-          <foreignObject x={toX(data.b / 10) - 12} y="0" width={60} height={50}>
-            <div style={{ fontSize: '12px', color: 'blue' }}>
-              {ppFrac([data.b, 10])} = {pp(data.b / 10)}
-            </div>
-          </foreignObject>
-          <text
-            x={toX(data.d)}
-            y={56}
-            fontSize={15}
-            textAnchor="middle"
-            stroke="blue"
-          >
-            ×
-          </text>
-          <text
-            x={toX(data.d)}
-            y={73}
-            fontSize={14}
-            textAnchor="middle"
-            stroke="blue"
-            strokeWidth={0.1}
-          >
-            {pp(data.d)}
-          </text>
-          <text
-            x={toX(data.c)}
-            y={56}
-            fontSize={15}
-            textAnchor="middle"
-            stroke="blue"
-          >
-            ×
-          </text>
-          <foreignObject x={toX(data.c) - 12} y="55" width={60} height={50}>
-            <div style={{ fontSize: '12px', color: 'blue' }}>
-              {ppFrac(data.c)} = {pp(data.c)}
-            </div>
-          </foreignObject>
-        </svg>
       </>
     )
   },
