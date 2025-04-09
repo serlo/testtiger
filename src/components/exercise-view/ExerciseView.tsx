193e430022e7f93dc14e5bba8a82d65bb065e2c9
Vitomir Budimir <budimirvitomir@gmail.com>
2025-04-09 11:56:21 +0200

    fix: apply position adjustments to ExerciseView as well

--- src/components/exercise-view/ExerciseView.tsx
+++ src/components/exercise-view/ExerciseView.tsx
@@ -4,13 +4,26 @@ import { useEffect } from 'react'
 import { setupExercise } from './state/actions'
 import { handleLearningPathStepClick } from '../learning-path/LearningPathMap'
 import { navigationData } from '@/content/navigations'
+import { PlayerProfileStore } from '@/store/player-profile-store'
 
 interface ExerciseViewProps {
   id: number
 }
 
 export function ExerciseView({ id }: ExerciseViewProps) {
+  // TODO: The following constants are duplicated from LearningPathMap,
+  // in order to avoid changing all positions manually in navigationData
+  const exam = PlayerProfileStore.useState(s => s.currentExam)
+  const path = navigationData[exam].path
   useEffect(() => {
+    const partVerticalOffset = 2400 // Offset pro Part (Themenpfad)
+    const additionalVerticalOffsetPerLesson = 80 // Zusätzlicher Offset pro Lesson innerhalb eines Parts
+    const imageOffset = 7020 - 45 // Vertikaler Offset für die Hintergrundbilder
+    const verticalScale = 1.1 // Skalierungsfaktor für y-Koordinaten der Elemente
+    const circleRadius = 50 // Standardkreisradius (außer bei Challenge)
+    const mapHeight =
+      navigationData[exam].mapHeight + partVerticalOffset * path.length
+
     if (ExerciseViewStore.getRawState().id !== id) {
       const hash = window.location.hash
       if (hash) {
@@ -18,10 +31,16 @@ export function ExerciseView({ id }: ExerciseViewProps) {
         console.log('parse it', obj)
         if (obj.lessonPosition) {
           navigationData[obj.exam].path.forEach((path, index) => {
-            path.lessons.forEach(lesson => {
+            const partOffset = index * partVerticalOffset
+            path.lessons.forEach((lesson, lessonIndex) => {
               if (
                 lesson.position?.x === obj.lessonPosition.x &&
-                lesson.position?.y === obj.lessonPosition.y
+                lesson.position?.y
+                  ? lesson.position.y +
+                      partOffset +
+                      lessonIndex * additionalVerticalOffsetPerLesson ===
+                    obj.lessonPosition.y
+                  : undefined
               ) {
                 obj.lesson = lesson
               }
